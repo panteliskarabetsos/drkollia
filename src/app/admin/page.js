c953011fcab6ac45bd1edd1511747224af5f47e2
Pantelis Karabetsos <panteliskarabetsos@Mac.home>
2025-10-07 14:43:29 +0300

    can add patients when offline and sync when back online

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState, useRef } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { supabase } from "../../lib/supabaseClient";
